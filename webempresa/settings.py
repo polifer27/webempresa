6cd79a1d5ac40f60495096fc6aac96c8aa0c4e5a
polifer27 <fernandezpablo27@yahoo.com.ar>
2023-10-06 14:50:20 +0000

    debug false

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i+wr02&1be59gqgcn=zxyy6u8rorc5le@+y65b1#650y!0bttb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [ 'pablofernandez.pythonanywhere.com' ]
-
+#ALLOWED_HOSTS = [ 'pablofernandez.pythonanywhere.com' ]
+ALLOWED_HOSTS = [ '*' ]
 
 # Application definition
 
